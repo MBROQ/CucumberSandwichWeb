093c09306de637c4ff98ee4395b9ce2796f1faf1
Mark Berryman <mark.berryman@roq.co.uk>
2023-02-20 14:59:26 +0000

    One Scenario passing with Selenium

--- features/support/Berryman's cookies.feature
+++ features/support/Berryman's cookies.feature
@@ -1,26 +1,28 @@
+Feature:Website
+
 Scenario: Title shows as expected
 Given Website is loaded
-Then Title shows as anticipated
+Then The title should be "Berrymans Buiscuits"
 
-Scenario: Font changes color after click
-Given Website is loaded
-When Button is clicked
-Then Font color changes
+#Scenario: Font changes color after click
+#Given Website is loaded
+#When Button is clicked
+#Then Font color changes
 
-Scenario: Picture loads
-When Website is loaded
-Then Picture shows as anticipated
+# Scenario: Picture loads
+# When Website is loaded
+# Then Picture shows as anticipated
 
-Scenario: Sub Headings load
-When Website is loaded
-Then Sub Headings show as anticipated
+# Scenario: Sub Headings load
+# When Website is loaded
+# Then Sub Headings show as anticipated
 
-Scenario: Text shows in form
-When Type in first name box
-Then writing shows in first name box
+# Scenario: Text shows in form
+# When Type in first name box
+# Then writing shows in first name box
 
-Scenario: Contact us link works
-Given Website is loaded
-When Contact us is clicked
-Then Sends to new page
+# Scenario: Contact us link works
+# Given Website is loaded
+# When Contact us is clicked
+# Then Sends to new page
 
--- features/support/steps.js
+++ features/support/steps.js
@@ -2,53 +2,52 @@ const assert = require('assert');
 const { Given, When, Then } = require('@cucumber/cucumber');
 const { Browser } = require('selenium-webdriver');
 
-const { After, Before, AfterAll, BeforeAll } = require('@cucumber/cucumber');
-Given("Website is loaded", function() {
-
-
-});
-
-When("Button is clicked", function() {
-
+Given("Website is loaded", function () {
+assert(true);
 
 });
 
-When("Type in first name box", function() {
-
+When("Button is clicked", function () {
+    
 
 });
 
-When("Contact us is clicked", function() {
+//When("Type in first name box", function () {
+    
 
+//});
+
+When("Contact us is clicked", function () {
+    
+
+})
+// Then The title should be "Berrymans Buiscuits"
+Then("The title should be {string}", function (title) {
+    assert(false);
 
 })
 
-Then("Title shows as anticipated", function() {
+//Then("Font color changes", function () {
+    
+
+//})
+
+//Then("Picture shows as anticipated", function () {
+    
+
+//})
+
+//Then("Sub Headings show as anticipated", function () {
 
 
-})
+//})
 
-Then("Font color changes", function() {
-
-
-})
-
-Then("Picture shows as anticipated", function() {
+//Then("writing shows in first name box", function () {
 
 
-})
+//})
 
-Then("Sub Headings show as anticipated", function() {
-
-
-})
-
-Then("writing shows in first name box", function() {
+//Then("Sends to new page", function () {
 
 
-})
-
-Then("Sends to new page", function() {
-
-
-})
+//})
